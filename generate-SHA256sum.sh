19057f67cb6aca2f09ac6135a17241b877a33c19
LeddaZ <leonardoledda@gmail.com>
2022-10-09 18:16:39 +0200

    Remove GitHub Actions workflows

--- generate-SHA256sum.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Generate SHA-256 hash
-sha256sum app/build/outputs/apk/release/app-release.apk > build/updater-SHA-256.txt
--- rename-updater.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Rename the signed APK
-mv app/build/outputs/apk/release/app-release-unsigned-signed.apk app/build/outputs/apk/release/app-release.apk
