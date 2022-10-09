35f5b6834e3f71421e989d32ea9df8ae67fb716d
LeddaZ <leonardoledda@gmail.com>
2022-10-09 18:15:44 +0200

    Migrate build scripts from Groovy to KTS

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "it.leddaz.revancedupdater"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "it.leddaz.revancedupdater"
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 17
+        versionName = "1.5.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.airbnb.android:paris:2.0.1")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.test:monitor:1.5.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
+    implementation("commons-codec:commons-codec:1.13")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
