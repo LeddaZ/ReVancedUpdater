b42274d6ea874795949f3d4f43b9f270379d762b
Leonardo Ledda <leonardoledda@gmail.com>
2023-05-10 00:34:00 +0200

    ReVancedUpdater: Bump version to 2.0.1
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 23
         targetSdk = 33
         versionCode = getCommitCount()
-        versionName = "2.0.0"
+        versionName = "2.0.1"
         resourceConfigurations += listOf("en", "it")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
