b02eb8cc5b1fb8eabce181b46336825a7019a899
Leonardo Ledda <leonardoledda@gmail.com>
2023-05-22 20:18:31 +0200

    ReVancedUpdater: Update dependencies
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.android.material:material:1.9.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0-alpha02" apply false
-    id("com.android.library") version "8.2.0-alpha02" apply false
+    id("com.android.application") version "8.2.0-alpha04" apply false
+    id("com.android.library") version "8.2.0-alpha04" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
 }
