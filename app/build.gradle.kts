75790f604914d228a586bd3e153babb55b597c0f
Leonardo Ledda <leonardoledda@gmail.com>
2023-10-10 20:57:53 +0200

    ReVancedUpdater: Upgrade AGP, Gradle and dependencies
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.android.volley:volley:1.2.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("commons-codec:commons-codec:1.16.0")
 }
