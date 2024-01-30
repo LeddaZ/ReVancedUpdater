80bb324f724c55a7a02e1fd7feb392a70f871d85
Leonardo Ledda <leonardoledda@gmail.com>
2024-01-30 22:24:59 +0100

    ReVancedUpdater: Bump version to 3.3.2
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = getCommitCount()
-        versionName = "3.3.1 (" + getCommitHash() + ")"
+        versionName = "3.3.2 (" + getCommitHash() + ")"
     }
 
     buildTypes {
